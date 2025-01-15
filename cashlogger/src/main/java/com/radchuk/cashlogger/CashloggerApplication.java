0afbb2fc4ba5b9e38413e72e513cb348b176243d
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2025-01-15 12:07:57 -0500

    first commit

---
+++ cashlogger/src/main/java/com/radchuk/cashlogger/CashloggerApplication.java
@@ -0,0 +1,13 @@
+package com.radchuk.cashlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashloggerApplication.class, args);
+    }
+
+}
---
+++ cashlogger/src/test/java/com/radchuk/cashlogger/CashloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radchuk.cashlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
